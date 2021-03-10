5ab6d82f1b2f3790bd7e0235f004f09f1d0a6b72
Elmar Atakishiyev <atakishiyev04@bk.ru>
2021-03-10 20:58:38 +0400

    Props Added In Classes

--- CSharpFinal3/Classes/Item.cs
+++ CSharpFinal3/Classes/Item.cs
@@ -8,5 +8,17 @@ namespace CSharpFinal3.Classes
 {
     class Item
     {
+        public string Product { get; set; }
+
+        public int Number { get; set; }
+        public readonly int Id;
+        private static int _id = 0;
+
+        public Item()
+        {
+            _id++;
+            Id = _id;
+        }
     }
 }
+
