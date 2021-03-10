21f931c144b0f92c8614a9d7afc51fd331606fc5
Elmar Atakishiyev <atakishiyev04@bk.ru>
2021-03-10 21:29:23 +0400

    Methods are added to Interfaces

--- CSharpFinal3/Interfaces/IMarketableProduct.cs
+++ CSharpFinal3/Interfaces/IMarketableProduct.cs
@@ -8,5 +8,19 @@ namespace CSharpFinal3.Interfaces
 {
     interface IMarketableProduct
     {
+        //Mehsullar
+        void Products();
+        //Satishdan Mehsulun Geri Qaytarilmasi
+        string ProductSales();
+        //Yeni Mehsul Elave Etmek
+        void AddProduct();
+        // Mehsulun adini,sayini ve meblegini,categoriyasini deyismek (code-a gore tapilacaq)
+        void ChangeProductInfo();
+        //Verilmis kateqoriyaya esasen hemin kateqoriyada olan mehsullarin qaytarilmasi
+        string ProductForCategory();
+        //Verilmis qiymet araligina esasen hemin araliqda olan mehsullarin qaytarilmasi
+        string ProductForPrice();
+        //Verilmis ada esasen mehsullarin search edilib qaytarilmasi
+         void ShowProductForName();
     }
 }
--- CSharpFinal3/Interfaces/IMarketableSales.cs
+++ CSharpFinal3/Interfaces/IMarketableSales.cs
@@ -8,5 +8,20 @@ namespace CSharpFinal3.Interfaces
 {
     interface IMarketableSales
     {
+        //Satishlar
+        void Sales();
+        //Satish Elave Etmek
+        void AddSales();
+
+        //Umumi satisin geri qaytarilmasi
+        string AllSales();
+        // Verilen tarix araligina gore hemin tarix araligina olan satislarin qaytarilmasi
+        string SalesForDate();
+        // Verilen bir tarixe gore hemin tarix (il,ay,gun) ucun olan satislarin qaytarilmasi
+        string SalesForExactDate();
+        //Verilmis mebleg araligina gore edilmis satislarin qaytarilmasi
+        string SalesForPrice();
+        //Verilmis nomreye esasen satisin qaytarilmasi
+        string SalesForNumber();
     }
 }
